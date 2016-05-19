9eea4fc54f6ecedf769245f80e562e523ce98edf
abhishek <abhishek.kumar11@sap.com>
2016-05-20 02:35:43 +0530

    Abhishek_kumar commit

--- AssetManager/platforms/android/assets/www/js/controllers.js
+++ AssetManager/platforms/android/assets/www/js/controllers.js
@@ -41,8 +41,22 @@ angular.module('starter.controllers', [])
        $ionicLoading.hide();
     $ionicSideMenuDelegate.isOpen() ? $ionicSideMenuDelegate.toggleLeft() : null; /// close side menu
   }
+  
+  
   $scope.createMovement = function(){
-    $scope.d1ata = "gregeelgele"
+    var ctrl = this;
+
+    ctrl.add = add;
+      
+    $http.get("/assetManagement/createMovement")
+            .success(function(data) {
+               console.log("Data " + data);
+            $ctrl.data = json.signify(data);
+            })
+            .error(function(data) {
+                alert("ERROR");
+            });
+      
     $ionicLoading.show({
           template: "Loading data..."
       })
@@ -51,6 +65,8 @@ angular.module('starter.controllers', [])
        $ionicLoading.hide();
     $ionicSideMenuDelegate.isOpen() ? $ionicSideMenuDelegate.toggleLeft() : null; /// close side menu
   }
+  
+  
   $scope.ruMovement = function(){
     
     $ionicLoading.show({
@@ -73,10 +89,11 @@ angular.module('starter.controllers', [])
     $ionicSideMenuDelegate.isOpen() ? $ionicSideMenuDelegate.toggleLeft() : null; /// close side menu
   }
   
+  
     $scope.checkAssets = function(){
         
           
-        $http.get("http://10.206.154.59:8080/assetManagement/testService")
+        $http.get("/assetManagement/testService")
             .success(function(data) {
                console.log("Data " + data);
             $scope.data11 = data;
@@ -92,14 +109,78 @@ angular.module('starter.controllers', [])
       console.log("hey")
       $state.go("app.checkAssets")
        $ionicLoading.hide();
-    $ionicSideMenuDelegate.isOpen() ? $ionicSideMenuDelegate.toggleLeft() : null; /// close side menu
-        
-           
-    
-    
-   
+    $ionicSideMenuDelegate.isOpen() ? $ionicSideMenuDelegate.toggleLeft() : null; /// close side menu 
   }
- 
+ // A.Kumar
+    $scope.openCity = function (evt, selectedTab) {           
+       
+        
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tabcontent.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    
+        
+    if(selectedTab == "see_report"){
+        document.getElementById("see_report").style.display = "block";
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(
+        function() {
+            
+            var a =12;
+            var b =28;
+            var c =43;
+            var d =25;
+            var e =91;
+
+        var data = google.visualization.arrayToDataTable([
+          ['Center', 'Total Items'],
+          ['Bangalore',    a],
+          ['Delhi',      b],
+          ['Chennai',  c],
+          ['Kochi', d],
+          ['Mysore',    e]
+        ]);
+
+        var options = {
+          title: 'Asset Details',
+          is3D: true
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('see_report'));
+         chart.draw(data, options);
+            
+        /*// orgChart is my global orgchart chart variable.
+            google.visualization.events.addListener(chart, 'select', selectHandler);
+            // Notice that e is not used or needed.
+            function selectHandler(e) {
+                alert('The user selected' + chart.getSelection().length + ' items.');
+      }*/
+        }
+      ); 
+    }else{
+        document.getElementById("see_report").style.display = "none";
+    }
+        
+        // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(selectedTab).style.display = "block";
+  //  evt.currentTarget.className += " active";
+        
+    
+}
+    
+    
  
 })
 .controller('ResultsCtrl', function($scope) {
--- AssetManager/www/js/controllers.js
+++ AssetManager/www/js/controllers.js
@@ -89,6 +89,18 @@ angular.module('starter.controllers', [])
     $ionicSideMenuDelegate.isOpen() ? $ionicSideMenuDelegate.toggleLeft() : null; /// close side menu
   }
   
+  $scope.CityAssets = function(){
+    
+    $ionicLoading.show({
+          template: "Loading data..."
+      })
+      console.log("hey")
+      $state.go("app.allMovement")
+       $ionicLoading.hide();
+    $ionicSideMenuDelegate.isOpen() ? $ionicSideMenuDelegate.toggleLeft() : null; /// close side menu
+  }
+  
+  
     $scope.checkAssets = function(){
         
           
@@ -108,14 +120,78 @@ angular.module('starter.controllers', [])
       console.log("hey")
       $state.go("app.checkAssets")
        $ionicLoading.hide();
-    $ionicSideMenuDelegate.isOpen() ? $ionicSideMenuDelegate.toggleLeft() : null; /// close side menu
-        
-           
-    
-    
-   
+    $ionicSideMenuDelegate.isOpen() ? $ionicSideMenuDelegate.toggleLeft() : null; /// close side menu 
   }
- 
+ // A.Kumar
+    $scope.openCity = function (evt, selectedTab) {           
+       
+        
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tabcontent.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    
+        
+    if(selectedTab == "see_report"){
+        document.getElementById("see_report").style.display = "block";
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(
+        function() {
+            
+            var a =12;
+            var b =28;
+            var c =43;
+            var d =25;
+            var e =91;
+
+        var data = google.visualization.arrayToDataTable([
+          ['Center', 'Total Items'],
+          ['Bangalore',    a],
+          ['Delhi',      b],
+          ['Chennai',  c],
+          ['Kochi', d],
+          ['Mysore',    e]
+        ]);
+
+        var options = {
+          title: 'Asset Details',
+          is3D: true
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('see_report'));
+         chart.draw(data, options);
+            
+        /*// orgChart is my global orgchart chart variable.
+            google.visualization.events.addListener(chart, 'select', selectHandler);
+            // Notice that e is not used or needed.
+            function selectHandler(e) {
+                alert('The user selected' + chart.getSelection().length + ' items.');
+      }*/
+        }
+      ); 
+    }else{
+        document.getElementById("see_report").style.display = "none";
+    }
+        
+        // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(selectedTab).style.display = "block";
+  //  evt.currentTarget.className += " active";
+        
+    
+}
+    
+    
  
 })
 .controller('ResultsCtrl', function($scope) {
